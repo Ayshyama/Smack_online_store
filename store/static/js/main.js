32bf4a1afdfafc76780aa6f9feb5ff6d701eec8b
Ayshyama@gmail.com <Ayshyama@gmail.com>
2023-06-07 23:20:23 +0200

    media width fixed

--- store/static/js/main.js
+++ store/static/js/main.js
@@ -45,3 +45,29 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+// Получаем ссылку на иконку для открытия корзины
+const openCartModalButton = document.getElementById('open-cart-modal');
+
+// Получаем ссылку на модальное окно корзины
+const cartModal = document.getElementById('cart-modal');
+
+// Получаем ссылку на элемент для закрытия модального окна
+const closeCartModalButton = document.getElementById('close-cart-modal');
+
+// Добавляем обработчик события клика на иконку открытия корзины
+openCartModalButton.addEventListener('click', function() {
+    cartModal.style.display = 'block';
+});
+
+// Добавляем обработчик события клика на кнопку закрытия модального окна
+closeCartModalButton.addEventListener('click', function() {
+    cartModal.style.display = 'none';
+});
+
+// Добавляем обработчик события клика на вне области модального окна для его закрытия
+window.addEventListener('click', function(event) {
+    if (event.target === cartModal) {
+        cartModal.style.display = 'none';
+    }
+});
